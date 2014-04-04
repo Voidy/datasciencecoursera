412b33ba4c26aed18a610aff2b826c884667cbda
Juggernoaf <handfulofvoid@gmail.com>
2014-04-04 18:33:44 +0600

    File listing

--- HipsterHound/Program.cs
+++ HipsterHound/Program.cs
@@ -7,13 +7,24 @@ namespace HipsterHound
 {
     class Program
     {
-        public static string musicPath = @"c:\Temp\huh";
+        public static string musicPath = @"c:\Temp\hurr";
 
 
         static void Main(string[] args)
         {
             // List existing music files, store them in the DB, and create a playlist out of them
-            //foreach (var file in  )            
-        }
+
+            if (!Directory.Exists(musicPath))
+            { 
+                foreach (var file in Directory.GetFiles(musicPath))
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The directory {0} does not exist or is inaccessible");
+            }
+        }    
     }
 }
